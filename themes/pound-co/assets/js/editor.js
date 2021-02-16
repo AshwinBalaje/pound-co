8092c641793a7dac37e00627c151a635978e29cb
Ashwin Balaje <ashwin.balaje@dcmail.ca>
2021-02-16 04:31:12 -0500

    Created custom block styling

--- themes/pound-co/assets/js/editor.js
+++ themes/pound-co/assets/js/editor.js
@@ -1,10 +1,78 @@
 wp.blocks.registerBlockStyle( 
     'core/button', 
     {
-        name: 'poundco-button',
-        label: 'PoundCo Button'
+        name: 'poundco-button1',
+        label: 'PoundCo Button 1'
     }
 );
+
+wp.blocks.registerBlockStyle( 
+    'core/button', 
+    {
+        name: 'poundco-button2',
+        label: 'PoundCo Button 2'
+    }
+);
+
+wp.blocks.registerBlockStyle( 
+    'core/cover', 
+    {
+        name: 'blur',
+        label: 'Blur'
+    }
+);
+
+
+
+//Text
+wp.blocks.registerBlockStyle( 
+    'core/paragraph', 
+    {
+        name: 'bahnschrift',
+        label: 'Bahnschrift'
+    }
+);
+
+wp.blocks.registerBlockStyle( 
+    'core/paragraph', 
+    {
+        name: 'lemon-bold',
+        label: 'Lemon Milk Bold'
+    }
+);
+
+wp.blocks.registerBlockStyle( 
+    'core/heading', 
+    {
+        name: 'lemon-medium',
+        label: 'Lemon Milk Medium'
+    }
+);
+
+wp.blocks.registerBlockStyle( 
+    'core/heading', 
+    {
+        name: 'lemon-medium-italic',
+        label: 'Lemon Milk Medium Italic'
+    }
+);
+
+wp.blocks.registerBlockStyle( 
+    'core/heading', 
+    {
+        name: 'lemon-bold',
+        label: 'Lemon Milk Bold'
+    }
+);
+
+wp.blocks.registerBlockStyle( 
+    'core/heading', 
+    {
+        name: 'lemon-bold-italic',
+        label: 'Lemon Milk Bold Italic'
+    }
+);
+
 
 wp.domReady( function() {
     wp.blocks.unregisterBlockStyle( 'core/quote', 'large' );
