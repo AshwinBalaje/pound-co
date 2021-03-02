bdfa5eae508795c84668bf66fe8ff191bd93a6ae
Ashwin Balaje <ashwin.balaje@dcmail.ca>
2021-03-02 01:26:51 -0500

    Modified product single styling

--- themes/pound-co/inc/woocommerce.php
+++ themes/pound-co/inc/woocommerce.php
@@ -1,2 +1,7 @@
 <?php
 add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 3 );
+
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
+
+
+
